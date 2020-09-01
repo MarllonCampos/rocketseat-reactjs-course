74166a6ab212cdca40cafef9f904752701c0245e
MarllonCampos <marlloncampos@outlook.com>
2020-09-01 00:57:52 -0300

    criado a estrutura e chamando alguns dados da api para popular o campo de produtos

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import Routes from './routes'
 
-
-import Main from './pages/main'
-
-
 import './styles.css'
 
-
 import Header from './components/Header'
+
 const App = () => {
   return (
     <>
--- src/pages/product/index.js
+++ src/pages/product/index.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router'
+
+import './styles.css'
+
+import api from '../../services/api'
 
 const Product = () => {
+    const [product, setProduct] = useState({})
+    const { id } = useParams()
+    useEffect(() => {
+        async function getApi() {
+            let response = await api.get(`/products/${id}`)
+            setProduct(response.data)
+        }
+        getApi()
+    })
+
     return (
-        <h1>Product </h1>
+        <div className="product-info">
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+            <p>
+                URL:<a href={product.url}>{product.url}</a>
+            </p>
+        </div>
     )
 }
 
